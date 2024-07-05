70f0a177c4c6c30a5e53f12c790f73599408bfa6
YStoyanovSogeti <yordan.stoyanov@sogeti.com>
2024-07-05 14:16:55 +0200

    fixed main layout & created expandable work history description

--- src/components/work-history/Workhistory.jsx
+++ src/components/work-history/Workhistory.jsx
@@ -1,29 +1,60 @@
 import './workhistory.css'
 import workHistoryData from '../../WorkhistoryData';
+import PropTypes from 'prop-types';
+import { useState } from 'react';
+
+const WorkHistoryItem = ({ item }) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const handleShowMoreLess = () => {
+    setIsExpanded(!isExpanded);
+  };
+
+  return (
+    <article className="cv-item">
+      <h3 className="cv-item__time">
+        <time dateTime={item.startDate}>{item.startDate}</time> - {item.endDate || 'current'}
+      </h3>
+      <dl className="cv-item__main">
+        <dt className="cv-item__heading">
+          <h4 className="cv-item__title">{item.title}</h4>
+          <h5 className="cv-item__location">{item.location}</h5>
+          <img className="cv-item__logo" src={`src/assets/${item.logoUrl}`} alt={`Logo of ${item.company}`} />
+        </dt>
+        {isExpanded 
+          ? <dd>
+              {item.description}
+            </dd>
+          : <dd className='line-clamp'>
+              {item.description}
+            </dd>
+        }
+        <button className='link' onClick={handleShowMoreLess}>
+          {isExpanded ? 'Show less' : 'Show more'}
+        </button>
+      </dl>
+    </article>
+  );
+};
 
 export default function WorkHistory() {
   return (
     <div>
       {workHistoryData.map((item, index) => (
-        <article key={index} className="cv-item">
-          <h3 className="cv-item__time">
-            <time dateTime={item.startDate}>{item.startDate}</time> - {item.endDate || 'current'}
-          </h3>
-          <dl className="cv-item__main">
-            <dt className="cv-item__heading">
-              <h4 className="cv-item__title">{item.title}</h4>
-              <h5 className="cv-item__location">{item.location}</h5>
-              <img className="cv-item__logo" src={`src/assets/${item.logoUrl}`} alt={`Logo of ${item.company}`} />
-            </dt>
-            <dd className="text-s">
-              {item.description}
-            </dd>
-          </dl>
-        </article>
+        <WorkHistoryItem key={index} item={item} />
       ))}
     </div>
   );
 }
 
-
-
+WorkHistoryItem.propTypes = {
+  item: PropTypes.shape({
+    startDate: PropTypes.string.isRequired,
+    endDate: PropTypes.string,
+    title: PropTypes.string.isRequired,
+    location: PropTypes.string.isRequired,
+    logoUrl: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    company: PropTypes.string.isRequired,
+  }).isRequired,
+};
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,14 +6,16 @@ function Home() {
   return (
     <>
       <Header></Header>
-      <section className="section section--dark">
-            <h2>Currently doing</h2>
-            <p>At the moment I am part of the front-end team for the Young Sogeti App. Here we develop a React application that provides Young Sogetists with relevant news and the ability to register for Young Sogeti events. Recently, I created a JavaScript calculator for the TMAP training sessions of the Sogeti Testing Community. I continuously update my knowledge and refine my skills, following the Sogeti Front-end Learning track to expand my expertise and stay current with the latest techniques.</p>
-      </section>
-      <section className="section section--dark">
-            <h2>Work history</h2>
-            <Workhistory></Workhistory>
-      </section>
+      <main>
+        <section className="section section--dark">
+              <h2>Currently doing</h2>
+              <p>At the moment I am part of the front-end team for the Young Sogeti App. Here we develop a React application that provides Young Sogetists with relevant news and the ability to register for Young Sogeti events. Recently, I created a JavaScript calculator for the TMAP training sessions of the Sogeti Testing Community. I continuously update my knowledge and refine my skills, following the Sogeti Front-end Learning track to expand my expertise and stay current with the latest techniques.</p>
+        </section>
+        <section className="section section--dark last-section">
+              <h2>Work history</h2>
+              <Workhistory></Workhistory>
+        </section>
+      </main>
     </>
   )
 }
